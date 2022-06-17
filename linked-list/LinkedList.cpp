18ef60e947698c6766d2206cb7df7e84dc82da47
ahsan722505 <ahsantahir722505@gmail.com>
2022-06-17 22:31:30 +0500

    ran

--- linked-list/LinkedList.cpp
+++ linked-list/LinkedList.cpp
@@ -16,6 +16,14 @@ public:
         List(){
             Head=Tail=0;
         }
+        List(List *l){
+            Head=Tail=0;
+            Node *p=l->Head;
+            while(p != 0){
+                this->addToTail(p->data);
+                p=p->next;
+            }
+        }
         bool isEmpty(){
             return Head == 0;
         }
@@ -125,16 +133,90 @@ public:
                 }
             }
         }
+        int recursiveMax(Node *p){
+            int x;
+            if(p == 0) return INT16_MIN;
+            else{
+                x=recursiveMax(p->next);
+                if(p->data > x) return p->data;
+                else return x;
+            }
+        }
+        Node* recursiveSearch(Node *p,const int key){
+            if(p == 0) return 0;
+            else if(p->data == key) return p;
+            return recursiveSearch(p->next,key);
+        }
+        bool isSorted(Node *p){
+            int x=INT8_MIN;
+            while(p != 0){
+                if(p->data < x) return false;
+                x=p->data;
+                p=p->next;
+            }
+            return true;
+        }
+        void removeDuplicateInSorted(Node *p){
+            Node *q=p->next;
+            while(q != 0){
+                if(p->data != q->data){
+                    p=q;
+                    q=q->next;
+                }else{
+                    p->next=q->next;
+                    delete q;
+                    q=p->next;
+                }
+            }
+        }
+        void reverseLinkedList(Node *p){
+            Tail=p;
+            Node *q=0;
+            Node *r=0;
+            while(p != 0){
+                r=q;
+                q=p;
+                p=p->next;
+                q->next=r;
+            }
+            Head=q;
+        }
+        void recursiveReverseLinkedList(Node *q,Node *p){
+                if(p != 0){
+                    recursiveReverseLinkedList(p,p->next);
+                    p->next=q;
+                    if(q == 0) Tail=p;
+                }else{
+                    Head=q;
+                }
+        }
+        List* concat(List *l2){
+            List *newl=new List(this);
+            List *newL2=new List(l2);
+            List *l3=new List;
+            l3->Head=newl->Head;
+            l3->Tail=newL2->Tail;
+            Node *p=l3->Head;
+            while(p->next !=0) p=p->next;
+            p->next=newL2->Head;
+            return l3;
+
+            
+        }
+        
 };
 int main(){
-    List l;
-    l.addToTail(1);
-    l.addToTail(2);
-    l.addToTail(3);
-    l.addToTail(4);
-    l.Display();
-    cout << '\n';
-    l.deleteSpecificNode(1);
-    l.Display();
+    List* l=new List;
+    l->addToTail(1);
+    l->addToTail(2);
+    l->addToTail(3);
+    l->addToTail(4);
+    List* l2=new List;
+    l2->addToTail(5);
+    l2->addToTail(6);
+    l2->addToTail(7);
+    l2->addToTail(8);
+    List* l3=l->concat(l2);
+    l3->recursiveDisplay(l3->Head);
     return 0;
 }
